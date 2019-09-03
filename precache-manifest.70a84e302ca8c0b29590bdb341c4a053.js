e71cb70caa5f81e3b32d137b3158d11093f7ae16
unknown <beth.mcguire1221@gmail.com>
2019-09-03 15:07:19 -0700

    Added all files

---
+++ precache-manifest.70a84e302ca8c0b29590bdb341c4a053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9764ffbf99c0fbf39f9d7bace3494c75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f47c97ca192225c394ba",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3554b580e38f54b7f83f",
+    "url": "/static/css/main.de5131ae.chunk.css"
+  },
+  {
+    "revision": "f47c97ca192225c394ba",
+    "url": "/static/js/2.bed8aaea.chunk.js"
+  },
+  {
+    "revision": "3554b580e38f54b7f83f",
+    "url": "/static/js/main.9c336a64.chunk.js"
+  },
+  {
+    "revision": "c2a283a126ecb1701e7a",
+    "url": "/static/js/runtime~main.d821bbcf.js"
+  }
+]);
